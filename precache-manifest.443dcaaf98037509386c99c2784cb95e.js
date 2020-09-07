280fafe6f03fce22cf898b8b14a235dd647decdc
Ray Maldonado <vivachihuahua2004@gmail.com>
2020-09-07 17:23:18 -0500

    Updates

---
+++ precache-manifest.443dcaaf98037509386c99c2784cb95e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7741f967d624daf33aad2e6de257667",
+    "url": "/react-full-stack-graphql/index.html"
+  },
+  {
+    "revision": "972e7eab7ecc0ed49a77",
+    "url": "/react-full-stack-graphql/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/react-full-stack-graphql/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "972e7eab7ecc0ed49a77",
+    "url": "/react-full-stack-graphql/static/js/main.4d9694f6.chunk.js"
+  },
+  {
+    "revision": "adba11e6b397ef38173d",
+    "url": "/react-full-stack-graphql/static/js/runtime~main.225aa185.js"
+  }
+]);
